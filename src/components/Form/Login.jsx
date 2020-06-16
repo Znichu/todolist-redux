e020dc558546cb68ffbbea22ee53e82da94e7906
Sergey Neplashov <znichu1989@gmail.com>
2020-06-16 16:47:49 +0300

    styling login form

--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -1,29 +1,17 @@
 import React from "react"
-import {Button, Icon, Modal} from "semantic-ui-react";
+import {Modal} from "semantic-ui-react";
 import LoginForm from "./LoginForm";
 
 
-
-
 class Login extends React.Component {
-    state = { modalOpen: true };
+    state = {modalOpen: true};
 
-    handleClose = () => this.setState({ modalOpen: false });
+    handleClose = () => this.setState({modalOpen: false});
 
     render() {
         return (
-            <Modal
-                open={this.state.modalOpen}
-                onClose={this.handleClose}
-                basic
-                size='small'
-            >
-<LoginForm login={this.props.login} />
-                <Modal.Actions>
-                    <Button color='green' onClick={this.handleClose} inverted>
-                        <Icon name='checkmark' /> Got it
-                    </Button>
-                </Modal.Actions>
+            <Modal open={this.state.modalOpen}  basic>
+                <LoginForm onClose={this.handleClose} login={this.props.login}/>
             </Modal>
         )
     }
--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import {useFormik} from 'formik';
+import {Button, Input} from "semantic-ui-react";
+import style from "./LoginForm.module.css"
 
 const SignupForm = (props) => {
     // Pass the useFormik() hook initial form values and a submit function that will
@@ -16,32 +18,47 @@ const SignupForm = (props) => {
         },
     });
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="email">Email Address</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            />
-            <label htmlFor="email">Password</label>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-            />
-            <input
-                id="rememberMe"
-                name="rememberMe"
-                type="checkbox"
-                onChange={formik.handleChange}
-                value={formik.values.rememberMe}
-            />
-            <button type="submit">Submit</button>
-        </form>
+        <div className={style.loginForm}>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                </div>
+                <div className={style.loginFormItem}>
+                    <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                </div>
+                <div className={style.loginFormItem}>
+                    <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                </div>
+                <div className={style.loginFormItem}>
+                    <input
+                        id="rememberMe"
+                        name="rememberMe"
+                        type="checkbox"
+                        onChange={formik.handleChange}
+                        value={formik.values.rememberMe}
+                    />
+                    <label htmlFor="password">remember me</label>
+                </div>
+
+
+                <Button color='blue' type="submit">Submit</Button>
+            </form>
+        </div>
     );
 };
 
